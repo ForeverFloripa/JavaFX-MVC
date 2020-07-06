ba4af5b513148d89434602da2b9be8193cec67b9
ForeverFloripa <34176505+ForeverFloripa@users.noreply.github.com>
2020-07-06 15:16:29 -0300

    p2

---
+++ JavaFX-MVC/src/javafxmvc/controller/FXMLVboxMainController.java
@@ -0,0 +1,40 @@
+package javafxmvc.controller;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.AnchorPane;
+
+public class FXMLVboxMainController implements Initializable {
+
+	@FXML
+	MenuItem mitCadastrosClientes;
+	@FXML
+	MenuItem mitProcessosVendas;
+	@FXML
+	MenuItem mitGraficosVendasMes;
+	@FXML
+	MenuItem mitEstoque;
+	@FXML
+	AnchorPane anchorpane;
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+	@FXML
+	public void handleMenuItemCadastroClientes() throws IOException {
+		AnchorPane a = (AnchorPane) FXMLLoader
+				.load(getClass().getResource("/javafxmvc/view/FXMLAnchorpaneCadastroClientes.fxml"));
+		anchorpane.getChildren().setAll(a);
+	}
+
+}
